47277728290e4b498fdee2e8bb822d0c7d27f9bb
Fibble <144193515+UBGHyper@users.noreply.github.com>
2024-10-25 17:03:45 +1100

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,14 +1,11 @@
 // This changes the title of your site
-
 var sitename = "UBGHyper"; // Change this to change the name of your website.
-var subtext = "v1.0"; // set the subtext
+var subtext = "v1.0"; // Set the subtext
 
-// more settings in main.css
-
-
+// More settings in main.css
 
 // END CONFIG
-// DO NOT MODIFY IF YOU DO NOT KNOW WHAT YOUR DOING!
+// DO NOT MODIFY IF YOU DO NOT KNOW WHAT YOU'RE DOING!
 
 import "/./config/custom.js";
 
@@ -28,9 +25,7 @@ function displayFilteredGames(filteredGames) {
     const gameImage = document.createElement("img");
     gameImage.src = `${serverUrl1}/${game.url}/${game.image}`;
     gameImage.alt = game.name;
-    gameImage.onclick = () => {
-      window.location.href = `play.html?gameurl=${game.url}/`;
-    };
+    gameImage.onclick = () => showGameCredits(game); // Updated to show credits
 
     const gameName = document.createElement("p");
     gameName.textContent = game.name;
@@ -41,6 +36,21 @@ function displayFilteredGames(filteredGames) {
   });
 }
 
+function showGameCredits(game) {
+  // Create a modal or a section to display the credits
+  const creditsModal = document.getElementById("creditsModal");
+  creditsModal.innerHTML = `
+    <h2>${game.name}</h2>
+    <p>Developer: ${game.credits}</p>
+    <button onclick="closeCredits()">Close</button>
+  `;
+  creditsModal.style.display = "block"; // Show the modal
+}
+
+function closeCredits() {
+  const creditsModal = document.getElementById("creditsModal");
+  creditsModal.style.display = "none"; // Hide the modal
+}
 
 function handleSearchInput() {
   const searchInputValue = document
@@ -52,7 +62,6 @@ function handleSearchInput() {
   displayFilteredGames(filteredGames);
 }
 
-
 fetch("./config/games.json") 
   .then((response) => response.json())
   .then((data) => {
@@ -61,11 +70,9 @@ fetch("./config/games.json")
   })
   .catch((error) => console.error("Error fetching games:", error));
 
-
 document
   .getElementById("searchInput")
   .addEventListener("input", handleSearchInput);
 
 document.getElementById("title").innerHTML = `${sitename}`;
-
-document.getElementById("subtitle").innerHTML = `${subtext}`
+document.getElementById("subtitle").innerHTML = `${subtext}`;
