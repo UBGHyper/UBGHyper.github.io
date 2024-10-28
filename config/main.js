80c03aa625515f4244a86292baec4b43fa7bbb17
Fibble <144193515+UBGHyper@users.noreply.github.com>
2024-10-28 12:03:36 +1100

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,84 +1,86 @@
-// This changes the title of your site
-var sitename = "UBGHyper"; 
-var subtext = "v1.0"; 
+document.addEventListener("DOMContentLoaded", () => {
+  // This changes the title of your site
+  var sitename = "UBGHyper"; 
+  var subtext = "v1.0"; 
 
-var serverUrl1 = "https://ubghyper.github.io/GameList.github.io/";
-var currentPageTitle = document.title;
-document.title = `${currentPageTitle} | ${sitename}`;
-let gamesData = []; 
+  var serverUrl1 = "https://ubghyper.github.io/GameList.github.io/";
+  var currentPageTitle = document.title;
+  document.title = `${currentPageTitle} | ${sitename}`;
+  let gamesData = []; 
 
-function displayFilteredGames(filteredGames) {
-  const gamesContainer = document.getElementById("gamesContainer");
-  gamesContainer.innerHTML = ""; 
+  function displayFilteredGames(filteredGames) {
+    const gamesContainer = document.getElementById("gamesContainer");
+    gamesContainer.innerHTML = ""; 
 
-  filteredGames.forEach((game) => {
-    const gameDiv = document.createElement("div");
-    gameDiv.classList.add("game");
+    filteredGames.forEach((game) => {
+      const gameDiv = document.createElement("div");
+      gameDiv.classList.add("game");
 
-    const gameImage = document.createElement("img");
-    gameImage.src = `${serverUrl1}/${game.url}/${game.image}`;
-    gameImage.alt = game.name;
-    gameImage.onclick = () => showGameCredits(game);
+      const gameImage = document.createElement("img");
+      gameImage.src = `${serverUrl1}/${game.url}/${game.image}`;
+      gameImage.alt = game.name;
+      gameImage.onclick = () => showGameCredits(game);
 
-    const gameName = document.createElement("p");
-    gameName.textContent = game.name;
+      const gameName = document.createElement("p");
+      gameName.textContent = game.name;
 
-    gameDiv.appendChild(gameImage);
-    gameDiv.appendChild(gameName);
-    gamesContainer.appendChild(gameDiv);
-  });
-}
+      gameDiv.appendChild(gameImage);
+      gameDiv.appendChild(gameName);
+      gamesContainer.appendChild(gameDiv);
+    });
+  }
 
-function showGameCredits(game) {
-  const creditsModal = document.getElementById("creditsModal");
-  creditsModal.innerHTML = `
-    <h2>${game.name}</h2>
-    <p>Developer: ${game.credits}</p>
-    <button onclick="closeCredits()">Close</button>
-  `;
-  creditsModal.style.display = "block"; 
-}
+  function showGameCredits(game) {
+    const creditsModal = document.getElementById("creditsModal");
+    creditsModal.innerHTML = `
+      <h2>${game.name}</h2>
+      <p>Developer: ${game.credits}</p>
+      <button onclick="closeCredits()">Close</button>
+    `;
+    creditsModal.style.display = "block"; 
+  }
 
-function closeCredits() {
-  const creditsModal = document.getElementById("creditsModal");
-  creditsModal.style.display = "none"; 
-}
+  function closeCredits() {
+    const creditsModal = document.getElementById("creditsModal");
+    creditsModal.style.display = "none"; 
+  }
 
-function handleSearchInput() {
-  const searchInputValue = document.getElementById("searchInput").value.toLowerCase();
-  const filteredGames = gamesData.filter((game) =>
-    game.name.toLowerCase().includes(searchInputValue)
-  );
-  displayFilteredGames(filteredGames);
-}
+  function handleSearchInput() {
+    const searchInputValue = document.getElementById("searchInput").value.toLowerCase();
+    const filteredGames = gamesData.filter((game) =>
+      game.name.toLowerCase().includes(searchInputValue)
+    );
+    displayFilteredGames(filteredGames);
+  }
 
-fetch("./config/games.json") 
-  .then((response) => response.json())
-  .then((data) => {
-    gamesData = data;
-    displayFilteredGames(data); 
+  fetch("./config/games.json") 
+    .then((response) => response.json())
+    .then((data) => {
+      gamesData = data;
+      displayFilteredGames(data); 
 
-    // Fetching details for the game from the URL parameter
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const gameUrl = urlParams.get("gameurl");
+      // Fetching details for the game from the URL parameter
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const gameUrl = urlParams.get("gameurl");
 
-    if (gameUrl) {
-      const gameData = data.find(game => game.url === gameUrl);
-      if (gameData) {
-        // Display game title and credits
-        document.getElementById("gameTitle").innerText = gameData.name;
-        document.getElementById("gameCredits").innerText = `Developer: ${gameData.credits}`;
-        // Update the iframe source
-        document.getElementById("gameFrame").src = `${serverUrl1}/${gameUrl}`;
-      } else {
-        document.getElementById("gameTitle").innerText = "Game Not Found";
-        document.getElementById("gameCredits").innerText = "Developer: Unknown";
+      if (gameUrl) {
+        const gameData = data.find(game => game.url === gameUrl);
+        if (gameData) {
+          // Display game title and credits
+          document.getElementById("gameTitle").innerText = gameData.name;
+          document.getElementById("gameCredits").innerText = `Developer: ${gameData.credits}`;
+          // Update the iframe source
+          document.getElementById("gameFrame").src = `${serverUrl1}/${gameUrl}`;
+        } else {
+          document.getElementById("gameTitle").innerText = "Game Not Found";
+          document.getElementById("gameCredits").innerText = "Developer: Unknown";
+        }
       }
-    }
-  })
-  .catch((error) => console.error("Error fetching games:", error));
+    })
+    .catch((error) => console.error("Error fetching games:", error));
 
-document.getElementById("searchInput").addEventListener("input", handleSearchInput);
-document.getElementById("title").innerHTML = `${sitename}`;
-document.getElementById("subtitle").innerHTML = `${subtext}`;
+  document.getElementById("searchInput").addEventListener("input", handleSearchInput);
+  document.getElementById("title").innerHTML = `${sitename}`;
+  document.getElementById("subtitle").innerHTML = `${subtext}`;
+});
